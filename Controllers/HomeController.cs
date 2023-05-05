f49a4973c3e5e4d4c7baa9fad8608d567d30b843
Arie De Bok <arie@directdebit.co.za>
2023-05-05 16:05:14 +0200

    Remove old controller action

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,11 +28,6 @@ namespace DotNetExample.Controllers
             return View();
         }
 
-        public IActionResult CreditCardForm()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
